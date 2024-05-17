2f0dfd1fb79868c7811170cc216b29b93111ced6
yoshiyoshi441 <2004902+yoshiyoshi441@users.noreply.github.com>
2024-05-18 01:04:07 +0900

    add

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      ["typescript-eslint"]: tseslint.plugin,
+    },
+    files: ["src/**/*.ts"],
+  },
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      semi: "error",
+    },
+  },
+];
