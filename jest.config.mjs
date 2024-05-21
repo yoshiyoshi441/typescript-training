48cc362a87c2c4b3670f8f47bf2c999e4691c705
yoshiyoshi441 <2004902+yoshiyoshi441@users.noreply.github.com>
2024-05-21 22:34:46 +0900

    update

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,9 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from "jest";
-
-const config: Config = {
+export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -200,5 +198,3 @@ const config: Config = {
   // Whether to use watchman for file crawling
   // watchman: true,
 };
-
-export default config;
